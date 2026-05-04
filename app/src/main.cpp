76bb854b96dd81c258c3bfee00157d599dbd795f
AR <rouetbii.ahmed@gmail.com>
2026-05-04 21:01:42 +0100

    added l4 t1

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,27 +3,26 @@
 #include <zephyr/logging/log.h>
  
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
-
+ 
+#define LED_NODE  DT_ALIAS(app_led)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-    bool led_state = true;
+    int ret=0;
+    ret +=  device_is_ready(led.port);
+    if(ret!=0){LOG_INF("Error Init the device");}
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+    ret +=gpio_pin_configure_dt(&led,GPIO_OUTPUT);
+    if(ret!=0){LOG_INF("Error Configuring  the device");}
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
-
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_BLINK_SLEEP_MS);
+ 
+        gpio_pin_toggle_dt(&led);
+        k_msleep(CONFIG_APP_HEART_BEAT_PERIOD);
     }
     return 0;
 }
